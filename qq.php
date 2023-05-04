8c48a88160bc200acd8c7db6a9b9f763ccc5e70c
kpk304kab <maks.salazkin@bk.ru>
2023-05-04 10:23:56 +0500

    add file project

---
+++ qq.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" class="form">
+        
+        <div class="form__field">
+          <input type="tel" name="name" placeholder="Телефон" pattern="89[0-9]{9}" minlength="1" maxlength="11" required />
+          <span class="form__error"></span>
+        </div>
+    
+     
+        <button type="submit">Отправить</button>
+        <style>
+input:invalid {border-color: red;}
+input:valid {border-color: green;}
+input:invalid:not(:placeholder-shown) {border-color: red;}
+input:valid:not(:placeholder-shown) {border-color: green;}
+
+<?php
+echo rand(), "\n";
+echo rand(), "\n";
+
+echo rand(5, 15), "\n";
+?>
+        </style>
+      </form> 
+</body>
+</html>
